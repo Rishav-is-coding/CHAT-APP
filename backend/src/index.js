1101c2a6cb8ad27edbcd45361c872dfdc026be42
Rishav-is-coding <rishavraj02012003@gmail.com>
2025-07-25 12:53:10 +0530

    feat: initialize backend with user authentication
    - Added package.json for backend dependencies and scripts.
    - Implemented user signup functionality in auth.controller.js.
    - Created user model with Mongoose in user.model.js.
    - Set up MongoDB connection in db.js.
    - Added JWT token generation and cookie handling in utils.js.
    - Established authentication routes in auth.route.js.
    - Configured Express server and middleware in index.js.

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+// const express = require("express")
+import express from "express"
+import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js";
+
+
+dotenv.config()
+const app = express();
+
+app.use(express.json())
+
+app.use("/api/auth" , authRoutes)
+
+app.listen(process.env.PORT || 5001 , () => {
+    console.log("Server running on PORT - ", process.env.PORT || 5001)
+    connectDB()
+})
