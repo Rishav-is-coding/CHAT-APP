5f21194b57c992090e411291e23e7221bbf59935
Rishav-is-coding <rishavraj02012003@gmail.com>
2025-07-25 17:54:22 +0530

    setup cloudinary, done with auth controllers and routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,16 @@
 import { generateToken } from "../lib/utils.js"
 import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body
     try{
         
+        if(!fullName || !email || !password){
+            res.status(400).json({message : "all fields are required"})
+        }
+
         if(password.length < 6) {
             return res.status(400).json({message : "password must be at least 6 characters"})
         }
@@ -43,10 +49,66 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    
+export const login = async (req, res) => {
+    const {email, password} = req.body
+    try{
+        const user = await User.findOne({email})
+        if(!user) return res.status(400).json({message : "invalid credentials"})
+
+        const isPasswordCorrect = await bcrypt.compare(password , user.password)
+        if(!isPasswordCorrect) {
+            return res.status(400).json({message : "invalid credentials"})
+        }
+
+        generateToken(user._id , res)
+
+        res.status(200).json({
+            _id : user._id,
+            fullName : user.fullName,
+            email: user.email,
+            profilePic : user.profilePic
+        })
+    }catch(error){
+        console.log("error in login controller" , error.message)
+        res.status(500).json({message : "internal server error"})
+    }
 }
 
 export const logout = (req, res) => {
-    
+    try {
+        res.cookie("jwt" , "" , {maxAge : 0})
+        res.status(200).json({message :"user logged out succesfully"})
+    }catch(error) {
+        console.log("error in logout controller : " , error.message)
+        res.status(500).json({message : "inernal server error"})
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id
+
+        if(!profilePic){
+            res.status(400).json({message : "profile pic not found"})
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        if(!uploadResponse) return res.status(500).json({message : "error while uploading to cloudinary"})
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic : uploadResponse.secure_url} , {new : true})
+        res.status(200).json(updatedUser)
+
+    }catch(error) {
+        console.log("error in updateProfile controller : " , error.message)
+        res.status(500).json({message : "internal server error"})
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try{
+        res.status(200).json(req.user)
+    }catch(error){
+        console.log("Error in checkAuth controller : " , error.message)
+        res.status(500).json({message : "internal server error"})
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
-
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 const app = express();
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json())
 
 app.use("/api/auth" , authRoutes)
+app.use(cookieParser())
 
 app.listen(process.env.PORT || 5001 , () => {
     console.log("Server running on PORT - ", process.env.PORT || 5001)
