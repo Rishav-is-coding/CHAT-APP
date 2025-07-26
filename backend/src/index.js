936e8aae6db57b17bfdacb2b659ec045cc56e752
Rishav-is-coding <rishavraj02012003@gmail.com>
2025-07-26 14:44:00 +0530

    set up socket.io for real-time communication and refactor server initialization

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,10 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import { app, server } from "./lib/socket.js";
 
 dotenv.config()
-const app = express();
+
 
 app.use(express.json())
 app.use(cookieParser())
@@ -20,7 +21,7 @@ app.use(cors({
 app.use("/api/auth" , authRoutes)
 app.use("/api/messages" , messageRoutes)
 
-app.listen(process.env.PORT || 5001 , () => {
+server.listen(process.env.PORT || 5001 , () => {
     console.log("Server running on PORT - ", process.env.PORT || 5001)
     connectDB()
 })
