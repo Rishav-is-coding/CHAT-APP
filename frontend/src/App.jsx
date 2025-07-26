d4435f2d5668b8eaf44437214e990e7149432945
Rishav-is-coding <rishavraj02012003@gmail.com>
2025-07-26 16:16:35 +0530

    fix: update document title and enhance favicon with custom SVG

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import SettingsPage from "./pages/SettingsPage.jsx"
 import ProfilePage from "./pages/ProfilePage.jsx"
 import { useAuthStore } from "./store/useAuthStore.js"
 import { useEffect } from "react"
-import { Loader } from "lucide-react"
+import { Loader, MessageSquare } from "lucide-react"
 import { Toaster } from "react-hot-toast"
 import { useThemeStore } from "./store/useThemeStore.js"
 
@@ -23,6 +23,28 @@ function App() {
   useEffect(() => {
     checkAuth()
   } , [checkAuth])
+
+   useEffect(() => {
+    const favicon = document.querySelector("link[rel='icon']");
+    const svgString = `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#7c3aed"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
+      </svg>
+    `;
+    const dataUri = `data:image/svg+xml,${encodeURIComponent(svgString)}`;
+    favicon.href = dataUri;
+  }, []);
+
 
   console.log({authUser})
 
